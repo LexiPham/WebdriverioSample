a6a703cf04e022c7580767ba2dc3b8e19a11bc83
LexiPham <phamkoko@gmail.com>
2020-02-01 14:13:41 +0700

    Update basic.js

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -3,7 +3,11 @@ const assert = require('assert')
 describe('webdriver.io page', () => {
     it('should have the right title', () => {
         browser.url('https://webdriver.io')
-        const title = browser.getTitle()
+        let title = browser.getTitle()
         assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
+        $('//a[contains(text(), "Guide")]').click()
+        title = browser.getTitle()
+        assert.strictEqual(title, 'Getting Started · WebdriverIO')
+//         assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js')
     })
 })
